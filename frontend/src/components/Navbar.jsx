6b4ce3be85f61aa7706cb0541fcf3f2292eecf61
Nana-AntwiD <darkwanana20@gmail.com>
2024-09-22 18:49:56 +0000

    frontend setup

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+
+        <img src={} alt="" /> 
+        
+
+    </div>
+  )
+}
+
+export default Navbar
